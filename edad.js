a56768f3580d88996f19f654a0248b9c46efa03d
El Booss <the.fentastore.cdmx@gmail.com>
2025-06-24 17:31:50 -0600

    Delete edad.js

--- edad.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const esMayor = confirm("¿Eres mayor de edad?");
-
-  if (!esMayor) {
-    document.getElementById("contenido").style.display = "none";
-    document.getElementById("bloqueado").style.display = "flex";
-  }
-});
